af0cf7fcdf7af41c0325788708170b5b3de6b394
Thomas Murray <thomas.murray09@gmail.com>
2020-09-24 23:26:28 +0200

    Website Styled

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -8,18 +8,14 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params["id"])
   end
 
-  # def new
-  #   @cocktail = Cocktail.new
-  #   # @ingredients = Ingredient.all
-  # end
-
   def create
     @cocktail = Cocktail.new(cocktail_params)
 
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
-      render :index
+      redirect_to cocktails_path
+      # render :show
     end
   end
 
@@ -27,11 +23,12 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params['id'])
     @cocktail.update(cocktail_params)
     redirect_to cocktail_path(@cocktail)
-    # if @cocktail.save
-    #   redirect_to cocktail_path(@cocktail)
-    # else
-    #   render :show
-    # end
+  end
+
+  def destroy
+    @cocktail = Cocktail.find(params['id'])
+    @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: %i[index create show update] do
+  resources :cocktails, only: %i[index create show update destroy] do
     collection do
     end
     member do
